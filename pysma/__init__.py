51c75294ebd64ba78659d1f1423e89a3891ee1ba
René Klomp <rene@klomp.ws>
2021-03-24 11:21:37 +0100

    Better operator for this comparison

--- pysma/__init__.py
+++ pysma/__init__.py
@@ -184,7 +184,7 @@ class Sensors:
             self.__s.remove(old)
             _LOGGER.warning("Replacing sensor %s with %s", old, sensor)
 
-        if sensor.key in self and self[sensor.key].key_idx is sensor.key_idx:
+        if sensor.key in self and self[sensor.key].key_idx == sensor.key_idx:
             _LOGGER.warning(
                 "Duplicate SMA sensor key %s (idx: %s)", sensor.key, sensor.key_idx
             )
