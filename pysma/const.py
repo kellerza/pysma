f28d52326b4a2368e3396577ba74aaa0ef949f4d
fitool <68473009+fitool@users.noreply.github.com>
2021-05-14 18:01:56 +0200

    Check &  re-order + reduce battery sensors
    For the two sensor groups below; I would rename them as follow to make in more clear and shorter (using 'power' to refer to 'active power') but not sure if this has an impact on existing naming in HA?
    SENSOR_METERING_ACTIVE_POWER_L1/2/3 --> SENSOR_METERING_POWER_FEEDIN_L1/2/3
    SENSOR_METERING_ACTIVE_POWER_CONSUMED_L1/2/3 --> SENSOR_METERING_POWER_DRAWN_L1/2/3
    
    On my Sunny Boy 5.0 PV inverter, I get a response of 60 times "{"val":null}" for the keys corresponding to the sensors below. I get "null" as I don't have optimizers, but I guess there is one value per PV panel (with a maximum of 60) --> if this is correct, it would mean the sensors only give a value for the first panel?
    SENSOR_OPTIMIZER_POWER 
    SENSOR_OPTIMIZER_CURRENT
    SENSOR_OPTIMIZER_VOLTAGE
    SENSOR_OPTIMIZER_TEMP

--- pysma/const.py
+++ pysma/const.py
@@ -35,9 +35,9 @@ FALLBACK_DEVICE_INFO = {
     "serial": "9999999999",
 }
 
-DEVCLASS_INVERTER = "1"
-DEVCLASS_BATTERY = "7"
-DEVCLASS_ENERGY_METER = "65"
+DEVCLASS_INVERTER = "1" # Solar Inverter
+DEVCLASS_BATTERY = "7" # Battery Inverter
+DEVCLASS_ENERGY_METER = "65" # Electricity meter
 OPTIMIZERS_VIA_INVERTER = "253"
 ENERGY_METER_VIA_INVERTER = "254"
 DEVICE_INFO = "255"
@@ -70,8 +70,40 @@ SENSOR_INVERTER_CONDITION = {
     SPATH: JMESPATHS_TAG,
     SL10N: True,
 }
+SENSOR_INVERTER_SYSTEM_INIT = {
+    SKEY: "6800_08811F00",
+    SNAME: "inverter_system_init",
+    SPATH: JMESPATHS_TAG,
+    SL10N: True,
+}
+SENSOR_GRID_CONNECTION_STATUS = {
+    SKEY: "6180_0846A700",
+    SNAME: "grid_connection_status",
+    SPATH: JMESPATHS_TAG,
+    SL10N: True,
+}
+SENSOR_GRID_RELAY_STATUS = {
+    SKEY: "6180_08416400",
+    SNAME: "grid_relay_status",
+    SPATH: JMESPATHS_TAG,
+    SL10N: True,
+}
 
-# DC side - DC measurements
+# Inverter operating time
+SENSOR_TOTAL_OPERATING_TIME = {
+    SKEY: "6400_00462E00",
+    SNAME: "total_operating_time",
+    SUNIT: "h",
+    SFACTOR: 3600,
+}
+SENSOR_TOTAL_FEEDIN_TIME = {
+    SKEY: "6400_00462F00",
+    SNAME: "total_feedin_time",
+    SUNIT: "h",
+    SFACTOR: 3600,
+}
+
+# DC side - DC measurements PV
 SENSOR_PV_POWER_A = {
     SKEY: "6380_40251E00_0",
     SNAME: "pv_power_a",
@@ -107,12 +139,83 @@ SENSOR_PV_CURRENT_B = {
     SFACTOR: 1000,
 }
 
-# AC Side - Grid measurements
+# AC Side - Grid measurements - (active) power
 SENSOR_GRID_POWER = {
     SKEY: "6100_40263F00",
     SNAME: "grid_power",
     SUNIT: "W",
 }
+SENSOR_POWER_L1 = {
+    SKEY: "6100_40464000",
+    SNAME: "power_l1",
+    SUNIT: "W",
+}
+SENSOR_POWER_L2 = {
+    SKEY: "6100_40464100",
+    SNAME: "power_l2",
+    SUNIT: "W",
+}
+SENSOR_POWER_L3 = {
+    SKEY: "6100_40464200",
+    SNAME: "power_l3",
+    SUNIT: "W",
+}
+
+# AC Side - Grid measurements - reactive power, apparent power and power factor
+SENSOR_GRID_REACTIVE_POWER = {
+    SKEY: "6100_40265F00",
+    SNAME: "grid_reactive_power",
+    SUNIT: "var",
+}
+SENSOR_GRID_REACTIVE_POWER_L1 = {
+    SKEY: "6100_40666000",
+    SNAME: "grid_reactive_power_l1",
+    SUNIT: "var",
+}
+SENSOR_GRID_REACTIVE_POWER_L2 = {
+    SKEY: "6100_40666100",
+    SNAME: "grid_reactive_power_l2",
+    SUNIT: "var",
+}
+SENSOR_GRID_REACTIVE_POWER_L3 = {
+    SKEY: "6100_40666200",
+    SNAME: "grid_reactive_power_l3",
+    SUNIT: "var",
+}
+SENSOR_GRID_APPARENT_POWER = {
+    SKEY: "6100_40666700",
+    SNAME: "grid_apparent_power",
+    SUNIT: "VA",
+}
+SENSOR_GRID_APPARENT_POWER_L1 = {
+    SKEY: "6100_40666800",
+    SNAME: "grid_apparent_power_l1",
+    SUNIT: "VA",
+}
+SENSOR_GRID_APPARENT_POWER_L2 = {
+    SKEY: "6100_40666900",
+    SNAME: "grid_apparent_power_l2",
+    SUNIT: "VA",
+}
+SENSOR_GRID_APPARENT_POWER_L3 = {
+    SKEY: "6100_40666A00",
+    SNAME: "grid_apparent_power_l3",
+    SUNIT: "VA",
+}
+SENSOR_GRID_POWER_FACTOR = {
+    SKEY: "6100_00665900",
+    SNAME: "grid_power_factor",
+    SUNIT: "",
+    SFACTOR: 1000,
+}
+SENSOR_GRID_POWER_FACTOR_EXCITATION = {
+    SKEY: "6180_08465A00",
+    SNAME: "grid_power_factor_excitation",
+    SPATH: JMESPATHS_TAG,
+    SL10N: True,
+}
+
+# AC Side - Grid measurements - frequency
 SENSOR_FREQUENCY = {
     SKEY: "6100_00465700",
     SNAME: "frequency",
@@ -120,7 +223,7 @@ SENSOR_FREQUENCY = {
     SFACTOR: 100,
 }
 
-# AC Side - Grid measurements - Phase currents
+# AC Side - Grid measurements - current
 SENSOR_CURRENT_L1 = {
     SKEY: "6100_40465300",
     SNAME: "current_l1",
@@ -146,7 +249,7 @@ SENSOR_CURRENT_TOTAL = {
     SFACTOR: 1000,
 }
 
-# AC Side - Grid measurements - Phase voltage
+# AC Side - Grid measurements - phase voltage
 SENSOR_VOLTAGE_L1 = {
     SKEY: "6100_00464800",
     SNAME: "voltage_l1",
@@ -166,24 +269,7 @@ SENSOR_VOLTAGE_L3 = {
     SFACTOR: 100,
 }
 
-# AC Side - Grid measurements - Active power
-SENSOR_POWER_L1 = {
-    SKEY: "6100_40464000",
-    SNAME: "power_l1",
-    SUNIT: "W",
-}
-SENSOR_POWER_L2 = {
-    SKEY: "6100_40464100",
-    SNAME: "power_l2",
-    SUNIT: "W",
-}
-SENSOR_POWER_L3 = {
-    SKEY: "6100_40464200",
-    SNAME: "power_l3",
-    SUNIT: "W",
-}
-
-# AC Side - Measured values
+# AC Side - Measured values - energy
 SENSOR_TOTAL_YIELD = {
     SKEY: "6400_00260100",
     SNAME: "total_yield",
@@ -194,18 +280,6 @@ SENSOR_DAILY_YIELD = {
     SKEY: "6400_00262200",
     SNAME: "daily_yield",
     SUNIT: "Wh",
-}
-SENSOR_TOTAL_OPERATING_TIME = {
-    SKEY: "6400_00462E00",
-    SNAME: "total_operating_time",
-    SUNIT: "h",
-    SFACTOR: 3600,
-}
-SENSOR_TOTAL_FEEDIN_TIME = {
-    SKEY: "6400_00462F00",
-    SNAME: "total_feedin_time",
-    SUNIT: "h",
-    SFACTOR: 3600,
 }
 
 # AC Side - Measured values - Grid measurements
@@ -278,7 +352,7 @@ SENSOR_METERING_VOLTAGE_L3 = {
     SFACTOR: 100,
 }
 
-# AC Side - Measured values - Active power
+# AC Side - Electricity meter - Measured values - Active power feed-in
 SENSOR_METERING_ACTIVE_POWER_L1 = {
     SKEY: "6100_0046E800",
     SNAME: "metering_active_power_l1",
@@ -295,7 +369,7 @@ SENSOR_METERING_ACTIVE_POWER_L3 = {
     SUNIT: "W",
 }
 
-# AC Side - Measured values - Active power consumed
+# AC Side - Electricity meter - Measured values - Active power drawn
 SENSOR_METERING_ACTIVE_POWER_CONSUMED_L1 = {
     SKEY: "6100_0046EB00",
     SNAME: "metering_active_power_consumed_l1",
@@ -320,7 +394,7 @@ SENSOR_PV_GEN_METER = {
     SFACTOR: 1000,
 }
 
-# Inverter Optimizers
+# PV Inverter Optimizers
 SENSOR_OPTIMIZER_SERIAL = {
     SKEY: "6800_10852600",
     SNAME: "optimizer_serial",
@@ -350,7 +424,7 @@ SENSOR_OPTIMIZER_TEMP = {
 }
 
 
-# Battery related sensors
+# Battery (inverter) related sensors
 SENSOR_BATTERY_VOLTAGE_A = {
     SKEY: "6100_00495C00_0",
     SNAME: "battery_voltage_a",
@@ -444,58 +518,6 @@ SENSOR_BATTERY_POWER_DISCHARGE_C = {
     SKEY: "6100_00499400_2",
     SNAME: "battery_power_discharge_c",
     SUNIT: "W",
-}
-SENSOR_GRID_REACTIVE_POWER = {
-    SKEY: "6100_40265F00",
-    SNAME: "grid_reactive_power",
-    SUNIT: "var",
-}
-SENSOR_GRID_REACTIVE_POWER_L1 = {
-    SKEY: "6100_40666000",
-    SNAME: "grid_reactive_power_l1",
-    SUNIT: "var",
-}
-SENSOR_GRID_REACTIVE_POWER_L2 = {
-    SKEY: "6100_40666100",
-    SNAME: "grid_reactive_power_l2",
-    SUNIT: "var",
-}
-SENSOR_GRID_REACTIVE_POWER_L3 = {
-    SKEY: "6100_40666200",
-    SNAME: "grid_reactive_power_l3",
-    SUNIT: "var",
-}
-SENSOR_GRID_APPARENT_POWER = {
-    SKEY: "6100_40666700",
-    SNAME: "grid_apparent_power",
-    SUNIT: "VA",
-}
-SENSOR_GRID_APPARENT_POWER_L1 = {
-    SKEY: "6100_40666800",
-    SNAME: "grid_apparent_power_l1",
-    SUNIT: "VA",
-}
-SENSOR_GRID_APPARENT_POWER_L2 = {
-    SKEY: "6100_40666900",
-    SNAME: "grid_apparent_power_l2",
-    SUNIT: "VA",
-}
-SENSOR_GRID_APPARENT_POWER_L3 = {
-    SKEY: "6100_40666A00",
-    SNAME: "grid_apparent_power_l3",
-    SUNIT: "VA",
-}
-SENSOR_GRID_POWER_FACTOR = {
-    SKEY: "6100_00665900",
-    SNAME: "grid_power_factor",
-    SUNIT: "",
-    SFACTOR: 1000,
-}
-SENSOR_GRID_POWER_FACTOR_EXCITATION = {
-    SKEY: "6180_08465A00",
-    SNAME: "grid_power_factor_excitation",
-    SPATH: JMESPATHS_TAG,
-    SL10N: True,
 }
 SENSOR_BATTERY_CHARGE_TOTAL = {
     SKEY: "6400_00496700",
@@ -603,111 +625,16 @@ SENSOR_BATTERY_TEMP_C = {
     SUNIT: "C",
     SFACTOR: 10,
 }
-SENSOR_INSULATION_RESISTANCE = {
-    SKEY: "6102_00254F00",
-    SNAME: "insulation_resistance",
-    SUNIT: "kOhm",
-    SFACTOR: 1000,
-}
-SENSOR_INSULATION_RESIDUAL_CURRENT = {
-    SKEY: "6102_40254E00",
-    SNAME: "insulation_residual_current",
-    SUNIT: "mA",
-}
-SENSOR_GRID_CONNECTIONS = {
-    SKEY: "6402_00666F00",
-    SNAME: "grid_connections",
-    SUNIT: "",
-}
-SENSOR_GRID_RELAY_STATUS = {
-    SKEY: "6180_08416400",
-    SNAME: "grid_relay_status",
-    SPATH: JMESPATHS_TAG,
-    SL10N: True,
-}
-SENSOR_BATTERY_CONDITION_A = {
-    SKEY: "6180_08414D00_0",
-    SNAME: "battery_condition_a",
-    SPATH: JMESPATHS_TAG,
-    SL10N: True,
-}
-SENSOR_BATTERY_CONDITION_B = {
-    SKEY: "6180_08414D00_1",
-    SNAME: "battery_condition_b",
-    SPATH: JMESPATHS_TAG,
-    SL10N: True,
-}
-SENSOR_BATTERY_CONDITION_C = {
-    SKEY: "6180_08414D00_2",
-    SNAME: "battery_condition_c",
-    SPATH: JMESPATHS_TAG,
-    SL10N: True,
-}
-SENSOR_BATTERY_CONDITION_TEMP_A = {
-    SKEY: "6180_08414E00_0",
-    SNAME: "battery_condition_temp_a",
-    SPATH: JMESPATHS_TAG,
-    SL10N: True,
-}
-SENSOR_BATTERY_CONDITION_TEMP_B = {
-    SKEY: "6180_08414E00_1",
-    SNAME: "battery_condition_temp_b",
-    SPATH: JMESPATHS_TAG,
-    SL10N: True,
-}
-SENSOR_BATTERY_CONDITION_TEMP_C = {
-    SKEY: "6180_08414E00_2",
-    SNAME: "battery_condition_temp_c",
-    SPATH: JMESPATHS_TAG,
-    SL10N: True,
-}
-SENSOR_BATTERY_STATUS_BMS_A = {
-    SKEY: "6182_08497F00_0",
-    SNAME: "battery_status_bms_a",
-    SPATH: JMESPATHS_TAG,
-    SL10N: True,
-}
-SENSOR_BATTERY_STATUS_BMS_B = {
-    SKEY: "6182_08497F00_1",
-    SNAME: "battery_status_bms_b",
-    SPATH: JMESPATHS_TAG,
-    SL10N: True,
-}
-SENSOR_BATTERY_STATUS_BMS_C = {
-    SKEY: "6182_08497F00_2",
-    SNAME: "battery_status_bms_c",
-    SPATH: JMESPATHS_TAG,
-    SL10N: True,
-}
 SENSOR_BATTERY_STATUS_OPERATING_MODE = {
     SKEY: "6180_08495E00",
     SNAME: "battery_status_operating_mode",
     SPATH: JMESPATHS_TAG,
     SL10N: True,
 }
-SENSOR_GRID_CONNECTION_STATUS = {
-    SKEY: "6180_0846A700",
-    SNAME: "grid_connection_status",
-    SPATH: JMESPATHS_TAG,
-    SL10N: True,
-}
-SENSOR_STATUS_FIRMWARE_UPDATE = {
-    SKEY: "6180_08412900",
-    SNAME: "status_firmware_update",
-    SPATH: JMESPATHS_TAG,
-    SL10N: True,
-}
-SENSOR_BATTERY_STATUS_SYSTEM_INIT = {
-    SKEY: "6800_08811F00",
-    SNAME: "battery_status_system_init",
-    SPATH: JMESPATHS_TAG,
-    SL10N: True,
-}
-SENSOR_BATTERY_STATUS_CHARGE_CONTROL_COMM = {
-    SKEY: "6182_08495600",
-    SNAME: "battery_status_charge_control_comm",
-    SPATH: JMESPATHS_TAG,
-    SL10N: True,
+SENSOR_INSULATION_RESIDUAL_CURRENT = {
+    SKEY: "6102_40254E00",
+    SNAME: "insulation_residual_current",
+    SUNIT: "mA",
 }
 
 
@@ -844,26 +771,13 @@ SENSOR_MAP = {
         SENSOR_BATTERY_TEMP_A,
         SENSOR_BATTERY_TEMP_B,
         SENSOR_BATTERY_TEMP_C,
-        SENSOR_INSULATION_RESISTANCE,
         SENSOR_INSULATION_RESIDUAL_CURRENT,
-        SENSOR_GRID_CONNECTIONS,
         SENSOR_INVERTER_CONDITION,
-        SENSOR_BATTERY_CONDITION_A,
-        SENSOR_BATTERY_CONDITION_B,
-        SENSOR_BATTERY_CONDITION_C,
-        SENSOR_BATTERY_CONDITION_TEMP_A,
-        SENSOR_BATTERY_CONDITION_TEMP_B,
-        SENSOR_BATTERY_CONDITION_TEMP_C,
         SENSOR_OPERATING_STATUS_GENERAL,
-        SENSOR_BATTERY_STATUS_BMS_A,
-        SENSOR_BATTERY_STATUS_BMS_B,
-        SENSOR_BATTERY_STATUS_BMS_C,
         SENSOR_BATTERY_STATUS_OPERATING_MODE,
         SENSOR_GRID_RELAY_STATUS,
         SENSOR_GRID_CONNECTION_STATUS,
-        SENSOR_STATUS_FIRMWARE_UPDATE,
-        SENSOR_BATTERY_STATUS_SYSTEM_INIT,
-        SENSOR_BATTERY_STATUS_CHARGE_CONTROL_COMM,
+        SENSOR_INVERTER_SYSTEM_INIT,
         SENSOR_GRID_POWER,
         SENSOR_VOLTAGE_L1,
         SENSOR_VOLTAGE_L2,
