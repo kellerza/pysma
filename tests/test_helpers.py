33b3893d01daf6c7d6b5ecd12f0ecd87a43f5c11
René Klomp <rene@klomp.ws>
2021-05-26 10:49:01 +0200

    test_version_int_to_string docstring

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -3,5 +3,6 @@ from pysma.helpers import version_int_to_string
 
 
 def test_version_int_to_string():
+    """Ensure version_int_to_string returns correct values."""
     assert version_int_to_string(51387396) == "3.10.28.R"
     assert version_int_to_string(None) == ""
