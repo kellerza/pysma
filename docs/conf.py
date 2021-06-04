55cef20e8bac9106757586b87a076d6eb221cefd
René Klomp <rene@klomp.ws>
2021-06-04 20:35:39 +0200

    Fix flake8 errors

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,5 @@
+"""Config for pysma documentation."""
+
 project = "PySMA"
 copyright = "2021, Johann Kellerman, René Klomp"
 author = "Johann Kellerman, René Klomp"
--- tests/test_definitions.py
+++ tests/test_definitions.py
@@ -5,7 +5,7 @@ from pysma.sensor import Sensor
 
 
 def test_duplicate_sensors():
-    """Test if defined sensors have unique key and name"""
+    """Test if defined sensors have unique key and name."""
     variables = vars(pysma.definitions)
     found_keys = []
     found_names = []
@@ -22,7 +22,7 @@ def test_duplicate_sensors():
 
 
 def test_sensor_map():
-    """Test if all map entries only contain unique items"""
+    """Test if all map entries only contain unique items."""
     for sensor_map in pysma.definitions.sensor_map.values():
         unique_items = list({f"{s.key}_{s.key_idx}": s for s in sensor_map}.values())
         assert unique_items == sensor_map
